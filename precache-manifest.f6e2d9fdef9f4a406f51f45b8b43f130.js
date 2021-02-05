202cdbf7bac1c64a87f49f805ac7ffc580e2f9c0
Huafu <1030363645@qq.com>
2021-02-05 13:54:53 +0800

    first

---
+++ precache-manifest.f6e2d9fdef9f4a406f51f45b8b43f130.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f58668106c733431c6965965a1c18e89",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9cbbd49ab05f18811f8",
+    "url": "/static/css/main.4a5e6854.chunk.css"
+  },
+  {
+    "revision": "8a46a02476d524eecef4",
+    "url": "/static/js/2.50d24118.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.50d24118.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9cbbd49ab05f18811f8",
+    "url": "/static/js/main.4eb44dfd.chunk.js"
+  },
+  {
+    "revision": "e4db7c41fb030e5ad0ee",
+    "url": "/static/js/runtime-main.ebb47c36.js"
+  }
+]);
